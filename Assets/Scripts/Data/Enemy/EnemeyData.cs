81c7cdd83449ce5e6e87124f6ae2ed24721e6715
DreamLU-2508 <121208687+DreamLU-2508@users.noreply.github.com>
2023-09-19 01:32:09 +0700

    enemy data

---
+++ Assets/Scripts/Data/Enemy/EnemeyData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DreamLU
+{
+    [CreateAssetMenu(fileName = "Enemey_", menuName = "Database/Enemy/Enemy Data")]
+    public class EnemeyData : ScriptableObject
+    {
+        public EnemeyID enemeyID;
+        public string enemyName;
+        public GameObject prefab;
+    }
+
+}
---
+++ Assets/Scripts/Data/Enemy/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace DreamLU
+{
+    [RequireComponent(typeof(SortingGroup))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(CircleCollider2D))]
+    [RequireComponent(typeof(PolygonCollider2D))]
+
+
+    [DisallowMultipleComponent]
+    public class Enemy : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
