d3b31e077d0fb58ad136266209df614e75dd36d4
DreamLU-2508 <121208687+DreamLU-2508@users.noreply.github.com>
2024-06-06 15:35:03 +0700

    Update WeaponData.cs

--- Assets/Scripts/Data/Weapon/WeaponData.cs
+++ Assets/Scripts/Data/Weapon/WeaponData.cs
@@ -10,15 +10,11 @@ namespace DreamLU
     {
         public Vector3 weaponShootPosition;
         
-        // Thời gian delay bắn của vũ khí
-        public float weaponPrechargeTime = 0f;
-
         [Header("Ammo Prefab")]
         public AmmoData ammoData;
 
         public int damage;
         public float cooldown;
-        public int timeReload;
 
         // [Header("Weapon Player")] 
         public WeaponPlayerBase _weaponPlayerBasePrefab;
